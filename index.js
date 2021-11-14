3e7dc3d4f7ca7a53fcb475c2b501f863d320ce92
Yaazieli <yaazieli@gmail.com>
2021-11-14 17:42:45 -0500

    commit tree 44798512aa6b0159e973a0b3c1cb5b6c3b5b5454

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(source, search) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === search.toLowerCase()
+    )
+  }
+
+  function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+
+  function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  }
